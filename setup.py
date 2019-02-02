719f8c776dbe8639ae3dfdebd958550805f1b48a
Michael Kaminsky <kaminsky.michael@gmail.com>
2019-02-02 17:28:58 -0600

    Adds CI for building and deploying to pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,35 @@
+import os
+import sys
 from setuptools import find_packages, setup
+from setuptools.command.install import install
 
 
 package_name = "dbt-helper"
-package_version = "0.0.2"
+VERSION = "0.0.3"
 description = """dbt-helper is a command line tool to help ease dbt development and database management"""
+
+
+class VerifyVersionCommand(install):
+    """
+    Custom command to verify that the git tag matches our version
+    https://circleci.com/blog/continuously-deploying-python-packages-to-pypi-with-circleci/
+    """
+
+    description = "verify that the git tag matches our version"
+
+    def run(self):
+        tag = os.getenv("CIRCLE_TAG")
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
 
 
 setup(
     name=package_name,
-    version=package_version,
+    version=VERSION,
     description=description,
     author="Michael Kaminsky",
     author_email="michael@kaminsky.rocks",
@@ -19,4 +40,5 @@ setup(
     entry_points={"console_scripts": ["dbt-helper = core.main:main"]},
     scripts=[],
     install_requires=["dbt"],
+    cmdclass={"verify": VerifyVersionCommand},
 )
