4eae6ede3f72bdc19a3014f13dba978412fb51cf
aescay <andrew.escay@gmail.com>
2020-10-08 16:13:38 -0400

    update tests

--- test/integration/006_source_schema_test/test_source_schemas.py
+++ test/integration/006_source_schema_test/test_source_schemas.py
@@ -4,13 +4,13 @@ from test.integration.base import DBTIntegrationTest
 class SourceSchemaTest(DBTIntegrationTest):
     def test_dependencies(self):
         self.run_dbt(["run"])
-        results = self.run_dbthelper(["show_upstream", "d"])
+        results = self.run_dbthelper(["show-upstream", "d"])
         self.assertTrue(len(results) == 5)
-        results = self.run_dbthelper(["show_downstream", "d"])
+        results = self.run_dbthelper(["show-downstream", "d"])
         self.assertTrue(len(results) == 1)
-        results = self.run_dbthelper(["show_upstream", "c"])
+        results = self.run_dbthelper(["show-upstream", "c"])
         self.assertTrue(len(results) == 4)
-        results = self.run_dbthelper(["show_downstream", "c"])
+        results = self.run_dbthelper(["show-downstream", "c"])
         self.assertTrue(len(results) == 2)
 
     def test_compare(self):
